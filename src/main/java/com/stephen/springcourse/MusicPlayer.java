fd6a82f1eae6910fa792a8a6a78c211ce46afa70
StephenLAB20 <stephenlab1@gmail.com>
2020-12-02 22:35:00 +0200

    Xml bean Dependency Injection (bean with list)

--- src/main/java/com/stephen/springcourse/MusicPlayer.java
+++ src/main/java/com/stephen/springcourse/MusicPlayer.java
@@ -1,29 +1,39 @@
 package com.stephen.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
 
-    private Music music;
+//    private Music music;
     private String name;
     private int volume;
+    private List<Music> musicList = new ArrayList<>();
 
     // empty constructor for spring // or remove your constructor if no needed
-
     public MusicPlayer() {}
-    // IoC
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+    // IoC
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
     }
+
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music song: musicList) {
+            System.out.println("Playing: " + song.getSong());
+        }
     }
 
     public void showProperties() {
-        System.out.println("Player: " + name + " Volume: " + volume);
+        System.out.println("Player: " + name + " | Volume: " + volume);
     }
 
-    public void setMusic(Music music) {
-        this.music = music;
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public String getName() {
